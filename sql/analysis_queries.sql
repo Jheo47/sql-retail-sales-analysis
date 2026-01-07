c05fda72f4a9b040e18aadd95f26cc49fd314080
Jheo47 <heo47@hotmail.com>
2026-01-07 08:14:47 +0000

    Update analysis_queries.sql

--- sql/analysis_queries.sql
+++ sql/analysis_queries.sql
@@ -77,4 +77,96 @@ FROM v_orders
 GROUP BY month
 ORDER BY month;
 
+-- For each Product Profit / sales / Margins---------------------------
+
+SELECT
+  category,
+  product_name,
+  SUM(quantity) AS total_units_sold,
+  ROUND(SUM(sales), 2)  AS total_sales,
+  ROUND(SUM(profit), 2) AS total_profit,
+  ROUND(SUM(profit) / SUM(sales) * 100, 2) AS profit_margin_pct
+FROM v_orders
+WHERE sales > 0
+GROUP BY category, product_name;
+
+
+-- TOP 30 PRODUCTS --------------------------------------------
+
+WITH product_sales AS (
+  SELECT
+    category,
+    product_name,
+    SUM(sales) AS total_sales,
+    SUM(profit) AS total_profit,
+    ROW_NUMBER() OVER (
+      PARTITION BY category
+      ORDER BY SUM(sales) DESC
+    ) AS sales_rank
+  FROM v_orders
+  GROUP BY category, product_name
+)
+SELECT
+  category,
+  'Top 30 Products' AS product_group,
+  ROUND(SUM(total_sales), 2) AS group_sales,
+  ROUND(SUM(total_profit), 2) AS group_profit,
+  ROUND(SUM(total_profit) / SUM(total_sales) * 100, 2) AS profit_margin_pct
+FROM product_sales
+WHERE sales_rank <= 30
+GROUP BY category;
+
+-- EVERYTHING WIHTOUT TOP 30------
+
+WITH product_sales AS (
+  SELECT
+    category,
+    product_name,
+    SUM(sales) AS total_sales,
+    SUM(profit) AS total_profit,
+    ROW_NUMBER() OVER (
+      PARTITION BY category
+      ORDER BY SUM(sales) DESC
+    ) AS sales_rank
+  FROM v_orders
+  GROUP BY category, product_name
+)
+SELECT
+  category,
+  'Rest of Category' AS product_group,
+  ROUND(SUM(total_sales), 2) AS group_sales,
+  ROUND(SUM(total_profit), 2) AS group_profit,
+  ROUND(SUM(total_profit) / SUM(total_sales) * 100, 2) AS profit_margin_pct
+FROM product_sales
+WHERE sales_rank > 30
+GROUP BY category;
+
+-- BOTTOM 30 products-------------------------------------------
+
+
+WITH product_sales AS (
+  SELECT
+    category,
+    product_name,
+    SUM(sales) AS total_sales,
+    SUM(profit) AS total_profit,
+    ROW_NUMBER() OVER (
+      PARTITION BY category
+      ORDER BY SUM(sales) ASC
+    ) AS sales_rank
+  FROM v_orders
+  GROUP BY category, product_name
+)
+SELECT
+  category,
+  'Bottom 30 Products' AS product_group,
+  ROUND(SUM(total_sales), 2) AS group_sales,
+  ROUND(SUM(total_profit), 2) AS group_profit,
+  ROUND(SUM(total_profit) / SUM(total_sales) * 100, 2) AS profit_margin_pct
+FROM product_sales
+WHERE sales_rank <= 30
+GROUP BY category;
+
+
+
 
